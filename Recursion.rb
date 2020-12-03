bd261544196cac5db07765364ded5a1f34683e35
buhlebaloyi <70333790+buhlebaloyi@users.noreply.github.com>
2020-12-03 11:30:59 +0200

    Update Recursion.rb

--- Recursion.rb
+++ Recursion.rb
@@ -3,19 +3,22 @@ def fibs(value = 1)
     number1 = 1
     number2 = 1
     if value == 1
-        print "01"
+        puts "01"
     else
-        until temp == value
+        stno = "11"
+        until temp == value - 1
             number = number1 + number2
             number2 = number1
             number1 = number
-            print number
+            stno = stno + number.to_s
             temp += 1
 
         end
         
 
     end
+
+    puts stno
 
 end
 
@@ -25,5 +28,10 @@ def fibs_rec(value)
 
 end
 
-a = fibs(4)
-b = fibs_rec(4)
+def printing_values(value)
+    (1..value).each { |values| puts fibs_rec(values) }
+
+end
+
+a = printing_values(5)
+b = fibs(5)
